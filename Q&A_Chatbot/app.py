879db01bcc9e5c7602f983c000dc686828067058
Chandragupt <chandraguptacharya02@gmail.com>
2026-02-13 14:30:53 +0530

    Q&A_Chatbot

--- Q&A_Chatbot/app.py
+++ Q&A_Chatbot/app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,12 +13,12 @@ os.environ["LANGCHAIN_PROJECT"]="Q&A Chatbot with ollama"
 
 #Prompt Template
 prompt=ChatPromptTemplate([
-    ("System","You are a helpful assitant. Please respont the user queries."),
+    ("system","You are a helpful assitant. Please respont the user queries."),
     ("user","Question:{question}")
 ])
 
 def generate_Response(question,llm,temperature,max_tokens):
-    llm=ollama(model=llm)
+    llm=Ollama(model=llm)
     output_parser=StrOutputParser()
     chain=prompt|llm|output_parser
     ans=chain.invoke({'question':question})
@@ -29,7 +29,7 @@ def generate_Response(question,llm,temperature,max_tokens):
 st.title("Q&A Chatbot with Ollama")
 
 #Settings Sidebar
-llm=st.sidebar.selectbox("Select your llm model",["mistral","gemma3"])
+llm=st.sidebar.selectbox("Select your llm model",["mistral"])
 temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
 max_tokens=st.sidebar.slider("Max_tokens",min_value=50,max_value=300,value=150)
 
